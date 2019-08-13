ad7cfada10548a384fd237c8aee5e372dda92b01
Kenia Rioja <kdrioja@gmail.com>
2019-08-13 16:27:46 -0700

    Exploring editText

--- app/src/main/java/com/example/interactivitydemo/MainActivity.java
+++ app/src/main/java/com/example/interactivitydemo/MainActivity.java
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     public void clickFunction(View view) {
-        Log.i("Info", "Blue Button Was Bressed.");
+        //findViewById returns a View so we need to cast it to an EditText object
+        EditText nameEditText = (EditText) findViewById(R.id.nameEditText);
+        Log.i("Info", "It worked!!!");
+        Log.i("Values", nameEditText.getText().toString());
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
